eedcf83bcd5f27d5707289ebd0aceb1d79eed232
Ricardo M. Lima <ricardolima89@gmail.com>
2021-03-09 10:16:30 -0300

    Snapshot testing Expense Dashboard and Not Found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => (
-    <div>404 - <Link to="/">Home</Link></div>
-)
+  <div>
+    404 - <Link to="/">Home</Link>
+  </div>
+);
+
+export { NotFound };

26d04ba9ef76357ae320dbe906934f9e413b0e6f
Ricardo M. Lima <ricardolima89@gmail.com>
2021-03-12 15:14:31 -0300

    Using connected components when accessed by router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Header } from "../components/Header";
 import { ExpenseDashboardPage } from "../components/ExpenseDashboardPage";
-import { AddExpensePage } from "../components/AddExpensePage";
-import { EditExpensePage } from "../components/EditExpensePage";
+import AddExpensePage from "../components/AddExpensePage";
+import EditExpensePage from "../components/EditExpensePage";
 import { HelpPage } from "../components/HelpPage";
 import { NotFound } from "../components/AddExpensePage";
 

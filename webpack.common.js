2a2447379dcfdb93ed82e29e68321b21241c20d3
Ricardo M. Lima <ricardolima89@gmail.com>
2021-05-22 12:41:40 -0300

    separete webpack configuration files - productionn & development

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
+
+// yarn add babel-core -> allows to use babel in tools like webpack
+// yarn add babel-loader -> teach webpack how to use

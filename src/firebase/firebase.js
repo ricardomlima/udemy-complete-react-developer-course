adebcc3f9c360a4d75a7a4d70efb1a23cc1e417c
Ricardo M. Lima <ricardolima89@gmail.com>
2021-06-27 18:34:50 -0300

    subscribing to changed data in firebase and turning into list

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,8 +17,7 @@ const database = app.database()
 
 // adding expenses
 database.ref("expenses")
-    .once("value")
-    .then((snapshot) => {
+    .on("value", (snapshot) => {
         const expenses = []
         snapshot.forEach((childSnapshot) => {
             expenses.push({

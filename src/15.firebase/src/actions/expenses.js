6a6bb3d682db2be4623a986e895970e5902345aa
Ricardo M. Lima <ricardolima89@gmail.com>
2021-07-01 12:03:40 -0300

    setting up async testing for firebase & redux operations

--- src/15.firebase/src/actions/expenses.js
+++ src/15.firebase/src/actions/expenses.js
@@ -30,7 +30,7 @@ const startAddExpense = (expenseData = {}) => {
 
     const expense = { description, note, amount, createdAt };
 
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
--- src/15.firebase/src/tests/actions/expenses.test.js
+++ src/15.firebase/src/tests/actions/expenses.test.js
@@ -1,5 +1,9 @@
-import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
-import expenses from '../fixtures/expenses'
+import configureMockStore from 'redux-mock-store';
+import thunk from "redux-thunk";
+import {  startAddExpense, addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 test("should setup remove expense action object", () => {
   const action = removeExpense({ id: "123abc" });
@@ -28,6 +32,20 @@ test("should setup add expense action object with provided values", () => {
     expense: expenses[2],
   });
 });
+
+test("should add expense to database and store", (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: "mouse",
+    amount: 3000,
+    notes: "this one is better",
+    createdAt: 1000
+  }
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    expect(1).toBe(1);
+    done();
+  })
+})
 
 // test("should setup add expense action object default values", () => {
 //   const action = addExpense();
